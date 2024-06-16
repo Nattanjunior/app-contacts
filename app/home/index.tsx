06e298b95ff91f6eb8f0f0a5f425f5c1ae15c413
nattanjunior <fnatanieljunior@gmail.com>
2024-06-16 04:38:55 -0300

    permissao pedida ao usuario para acessar os contatos dele feita, coleta dos dados dos contatos feita e exibida na tela do usuario.

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,15 +1,67 @@
-import { View,Text ,TouchableOpacity} from "react-native";
+import { View,TouchableOpacity, Alert,SectionList,Text, SectionListData} from "react-native";
 import { styles } from "./style";
 import { Input } from "../../components/input";
-
+import * as Contacts from 'expo-contacts'
+import { useEffect, useState,useId } from "react";
 import {Feather} from '@expo/vector-icons'
 import { theme } from "../../theme";
-import { useState } from "react";
 
-import { Contact } from "../../components/contact";
+import { Contact, ContactProps } from "../../components/contact";
+
+
+type SectionListDataProps = {
+   title: string,
+   data: ContactProps[]
+}
 
 export default function Home(){
-    const [name,setName] = useState("");
+ const [contacts, setContacts] = useState<SectionListDataProps[]>([])
+ const [name,setName] = useState("");
+
+ async function fecthContacts() {
+   try {
+      //permissão para acessar os contatos do usuário.
+      const { status} = await Contacts.requestPermissionsAsync();
+      //verificando se foi permitido ou não.
+      if(status === Contacts.PermissionStatus.GRANTED){
+         //pegando os contatos do usuário.
+         const {data} = await Contacts.getContactsAsync({sort: "firstName"});
+         //formatando os dados recebidos
+         const list  = data.map((contacts)=> ({
+            id: contacts.id ?? useId(),
+            name: contacts.name,
+            image: contacts.image
+         })).reduce<SectionListDataProps[]>((acc:any, item) => {
+            //pegando a primeira letra do nome do contato do usuário
+            const firstLetter = item.name[0].toUpperCase();
+
+            const existingEntry = acc.find((entry:SectionListDataProps) => {
+               entry.title === firstLetter
+            })
+
+            if(existingEntry){
+               existingEntry.data.push(item);       
+            }else{
+               acc.push({title: firstLetter, data: [item]})
+            }
+
+
+            return acc
+         },[])
+          
+         setContacts(list)
+      }
+
+   } catch (error) {
+      console.error(error);
+      Alert.alert("Contatos", "Não foi possível carregar os contatos.")
+   }
+ }
+
+ useEffect(()=>{
+ fecthContacts()
+ },[])
+
  return(
  <View style={styles.container}>
     <View style={styles.header}>
@@ -28,9 +80,27 @@ export default function Home(){
 
     </View>
 
-    <Contact contact={{
-      name:"Nattan", 
-      image: require("../../assets/images/image-me.jpeg") }} />
+    
+
+
+    <SectionList 
+     sections={contacts} 
+     keyExtractor={(item) => item.id}
+     renderItem={({item})=> (
+      <Contact 
+       contact={item} />
+      )}
+
+      //renderizando as sections(sessões)
+      renderSectionHeader={({section})=> <Text style={styles.section}>{section.title}</Text>}
+    
+    //faazendo estilização dentro da lista
+    contentContainerStyle={styles.contentList}
+    
+    />
+
+    
+
  </View>
 )
 }
--- theme/colors.ts
+++ theme/colors.ts
@@ -4,8 +4,8 @@ export const colors = {
  blue: '#4442F0',
 
  gray_100:"#EBEBEB" ,
- gray_200: "C4C4CC",
- gray_300: "8D8D99",
- gray_400: "7C7C8A",
- gray_800:"161616",
+ gray_200: "#C4C4CC",
+ gray_300: "#8D8D99",
+ gray_400: "#7C7C8A",
+ gray_800:"#161616",
 }
